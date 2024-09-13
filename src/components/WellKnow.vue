83d9ad9efadd7ea529ae5b63997eb5edf98cdb8b
Dung Lu <dung@mylong.io>
2024-09-13 13:54:53 +0700

    Add

---
+++ src/components/WellKnow.vue
@@ -0,0 +1,31 @@
+<template>
+  <pre>{{ formattedJson }}</pre>
+</template>
+
+<script>
+
+import assetLinks from '../assets/assetlinks.json'; // Adjust path accordingly
+
+export default {
+  data() {
+    return {
+      jsonData: assetLinks,
+    };
+  },
+  computed: {
+    formattedJson() {
+      return JSON.stringify(this.jsonData, null, 2);
+    },
+  },
+};
+</script>
+
+<style>
+pre {
+  background: #f4f4f4;
+  padding: 10px;
+  border-radius: 4px;
+  white-space: pre-wrap;
+  word-wrap: break-word;
+}
+</style>
